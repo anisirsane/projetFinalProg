fab52ea3ac8a69a5a9296c998a2c5cabe6590931
anisirsane <anis.irsane2001@gmail.com>
2023-04-10 02:25:37 -0300

    Version Finale

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+/*
+  Titre      : projet finale programmation embarque et web
+  Auteur     : Anis Irsane
+  Date       : 09-04-2023
+  Description: un prjet ou on recupere les donnees depuis un BME280 en utilisant le protocole I2C
+               -on les envoie sur une page html qui est sur un serveur locale
+               - on fait les mises a jour des firmware avec OTA
+               -l'envoie des donnees est avec le protocole HTTP sou forme d'un objet JSON
+               -recuperation des donnees avec ajax et affichage sur notre page html
+               -la page html est ecrit avec le html, css, bootstrao et javascript
+               - la page est mise a jour chaque 3 secondes .
+  Version    : 0.0.1
+*/
+
 //libraries necessaires
 #include <Arduino.h>
 #include <WiFi.h>
@@ -8,30 +22,28 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <AsyncElegantOTA.h>
+
 //partie de declaration des variables
 #define SEALEVELPRESSURE_HPA (1013.25)
-#define I2C_SDA 21 
-#define I2C_SCL 22
-// Declaration object 
+
+// Declaration de notre objet BME
 Adafruit_BME280 bme; // I2C
 
-
-
+//configuration de wifi
 //const char* ssid = "UNIFI_IDO1";
 //const char* password = "42Bidules!";
 const char* ssid = "Ste-adele";
 const char* password = "allo1234";
+
+//declaration des variables
 float temperature, pressure,humidity, altitude ; 
+unsigned long PreviousTime = 0; 
+const int DELAY = 3000;
 
-
-
+//lancement de notre serveur
 AsyncWebServer server(80);
 
-
-
-
-// Declation de la constante qui contient la page HTML 
-
+// Declaration de la constante qui contient la page HTML 
 const char fichierhtml[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -41,7 +53,7 @@ const char fichierhtml[] PROGMEM = R"rawliteral(
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">  
   </head>
   <body>
-    <!--c'est pour la premiere partie de la page web-->
+
   <div class="firstimage text-center row">
       <div class="row firstrow pb-4 mb-2 ml-0 mr-0">
         <h3>BME280 WEB SERVER</h3>
@@ -49,10 +61,8 @@ const char fichierhtml[] PROGMEM = R"rawliteral(
       <div class="row pb-4 ">
         <div class="rowdata">
           <div class="row pb-2 justify-content-md-center ">
-            <div class="col-md-2">
-              <img src="temp.svg">
-            </div>
-            <div class="col-md-4" >
+
+            <div class="col-sm" >
 
               <h5 class="tempval"><b>TEMPERATURE</b></h5>
 
@@ -69,10 +79,8 @@ const char fichierhtml[] PROGMEM = R"rawliteral(
       <div class="row pb-4 ">
         <div class="rowdata">
           <div class="row pb-2 justify-content-md-center " >
-            <div class=" col-md-2" >
-              <img src="humidity.svg">
-            </div>
-            <div class=" col-md-4" >
+
+            <div class=" col-sm" >
 
               <h5 class="humidityval"><b>HUMIDITY</b></h5>
 
@@ -88,10 +96,8 @@ const char fichierhtml[] PROGMEM = R"rawliteral(
     <div class="row pb-4 ">
       <div class="rowdata">
         <div class="row pb-2 justify-content-md-center " >
-          <div class="col-md-2">
-            <img src="arrows.svg">
-          </div>
-          <div class="col-md-4">
+
+          <div class="col-sm">
 
             <h5 class="preval"><b>PRESSURE</b></h5>
 
@@ -108,10 +114,7 @@ const char fichierhtml[] PROGMEM = R"rawliteral(
     <div class="row pb-4 ">
       <div class="rowdata">
         <div class="row pb-2 justify-content-md-center ">
-          <div class=" col-md-2">
-            <img class="wind" src="winds.svg">
-          </div>
-          <div class=" col-md-4">
+          <div class=" col-sm">
 
             <h5 class="altval"><b>ALTITUDE</b></h5>
 
@@ -127,10 +130,9 @@ const char fichierhtml[] PROGMEM = R"rawliteral(
   </div>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <script >
-    // function to fetch data from ESP32 and update the form
+    // fonction pour recuperer les donnees et les afficher sur notre forme
     function updateData() {
       $.ajax({
         url: '/data',
@@ -170,10 +172,6 @@ const char fichierhtml[] PROGMEM = R"rawliteral(
 	border-radius: 20px 20px 0 0;
 	margin:auto 0 auto 0;
 }
-img{
-	width:10px;
-	height: auto;
-}
 .tempval{
 	color:#28716a;
 }
@@ -204,7 +202,10 @@ img{
   
 </body>
 </html>)rawliteral"; 
+//fin de notre page html
+
 void setup() {
+
   Serial.begin(9600);
   Serial.println(F("BME280 test"));
 
@@ -216,48 +217,44 @@ void setup() {
   }
   Serial.println(WiFi.localIP());
 
+  //lancement et detection bme280
+  bool status = bme.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  
+  }
 
-
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-
-//sources : https://randomnerdtutorials.com/esp32-async-web-server-espasyncwebserver-library
-//https://raphaelpralat.medium.com/example-of-json-rest-api-for-esp32-4a5f64774a05#:~:text=Once%20connected%2C%20display%20the%20ESP32%20IP%20address%2C%20it,%28AsyncWebServerRequest%20%2Arequest%29%20%7B%20request-%3Esend%20%28200%2C%20%22application%2Fjson%22%2C%20%22%20%7B%22message%22%3A%22Welcome%22%7D%22%29%3B
+  //sources : https://randomnerdtutorials.com/esp32-async-web-server-espasyncwebserver-library
+  //https://raphaelpralat.medium.com/example-of-json-rest-api-for-esp32-4a5f64774a05#:~:text=Once%20connected%2C%20display%20the%20ESP32%20IP%20address%2C%20it,%28AsyncWebServerRequest%20%2Arequest%29%20%7B%20request-%3Esend%20%28200%2C%20%22application%2Fjson%22%2C%20%22%20%7B%22message%22%3A%22Welcome%22%7D%22%29%3B
+  //affichage de notre page html sur le url "/"
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
       request->send(200, "text/html", fichierhtml);  // chargement page html
     });
+  //envoie de notre objet json sur le url "/data"
   server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
       String json = "{\"temperature\":" + String(temperature) + ",\"pressure\":" + String(pressure) + ",\"altitude\":" + String(altitude) + ",\"humidity\":" + String(humidity) + "}";
 
     request->send(200, "application/json", json);
   });
 
-
-    AsyncElegantOTA.begin(&server);    // Start serveur ElegantOTA
+  // Start serveur ElegantOTA
+    AsyncElegantOTA.begin(&server); 
+  
+  // Lancement de serveur
     server.begin();
     Serial.println("HTTP server started");
 
-
-
-
-
 }
 void loop() {
-   //verification si on detecte un bme280
-  bool status = bme.begin(0x76);  
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-  
-  }
-  // Read BME280 sensor data
-  temperature = bme.readTemperature();
-  pressure = bme.readPressure() / 100.0F;
-  altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  humidity = bme.readHumidity();
-  Serial.println(temperature);
-  Serial.println(pressure);
-  Serial.println(altitude);
-  Serial.println(humidity);
-  delay(3000);
 
+  //on lit les donnees depuis le bme280 chaque 3 seconde 
+  if (millis() - PreviousTime > DELAY)
+  {
+    temperature = bme.readTemperature();
+    pressure = bme.readPressure() / 100.0F;
+    altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    humidity = bme.readHumidity();
+    PreviousTime = millis();
+  }
+  
 }
